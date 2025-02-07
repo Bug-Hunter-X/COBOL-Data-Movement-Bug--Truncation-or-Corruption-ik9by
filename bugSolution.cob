7948c09c5cca8c30bd3fae66db0a27d84f74af5b
BugHunter-X <askzenai@gmail.com>
2025-02-07 23:38:05 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,12 @@
+01  WS-AREA-1 PIC X(100). 
+01  WS-AREA-2 PIC X(100). 
+
+PROCEDURE DIVISION.
+    MOVE 'Hello' TO WS-AREA-1
+    IF LENGTH OF WS-AREA-1 > LENGTH OF WS-AREA-2 THEN
+        DISPLAY "Error: Receiving area too short!" 
+        STOP RUN
+    END-IF
+    MOVE WS-AREA-1 TO WS-AREA-2
+    DISPLAY WS-AREA-2
+    GOBACK.
